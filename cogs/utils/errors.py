29b62e35e45779554aadec9e608deb2e3e26b200
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-11-30 18:35:47 -0700

    Update errors.py

--- cogs/utils/errors.py
+++ cogs/utils/errors.py
@@ -7,51 +7,77 @@ class ErrorsCog(commands.Cog, name = "Errors"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener('on_command_error')
-    async def command_error(self, ctx, error):
+    async def send_to_ctx_or_author(self, ctx, text: str = None, *args, **kwargs) -> typing.Optional[discord.Message]:
+        """Tries to reply the given text to ctx, but failing that, tries to reply it to the author
+        instead. If it fails that too, it just stays silent."""
+
+        try:
+            return await ctx.send(text, *args, **kwargs)
+        except discord.Forbidden:
+            try:
+                return await ctx.author.send(text, *args, **kwargs)
+            except discord.Forbidden:
+                pass
+        except discord.NotFound:
+            pass
+        return None
+
+    @Cog.listener()
+    async def on_command_error(self, ctx, error):
+        ignored_errors = (commands.CommandNotFound, commands.NotOwner)
+        
+        error = getattr(error, "original", error)
+
+        if isinstance(error, ignored_errors):
+            return
+
+        setattr(ctx, "original_author_id", getattr(ctx, "original_author_id", ctx.author.id))
+        owner_reinvoke_errors = (
+            commands.MissingAnyRole, commands.MissingPermissions,
+            commands.MissingRole, commands.CommandOnCooldown, commands.DisabledCommand
+        )
+
+        if ctx.original_author_id in self.bot.owner_ids and isinstance(error, owner_reinvoke_errors):
+            return await ctx.reinvoke()
         if isinstance(error, commands.CommandNotFound):
             return
         elif isinstance(error, commands.MissingPermissions):
-            await ctx.reply(embed=discord.Embed(description=str(error), color=discord.Color.red()))
+            await ctx.send(embed = discord.Embed(title = str(error), color = discord.Color.red()))
         elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.reply(embed=discord.Embed(description=str(error), color=discord.Color.red()))
+            await ctx.send(embed = discord.Embed(title = str(error), color = discord.Color.red()))
         elif isinstance(error, commands.BadArgument):
-            await ctx.reply(embed=discord.Embed(description=str(error), color=discord.Color.red()))
-        elif isinstance(error, commands.NotOwner):
-            await ctx.reply(embed=discord.Embed(description="You are not an owner.", color=discord.Color.red()))
+            await ctx.send(embed = discord.Embed(title = str(error), color = discord.Color.red()))
+        #elif isinstance(error, commands.NotOwner):
+            #await ctx.send(embed = discord.Embed(title = "You are not an owner.", color = discord.Color.red()))
         elif isinstance(error, commands.BotMissingPermissions):
-            await ctx.reply(embed=discord.Embed(description=str(error), color=discord.Color.red()))
-        elif isinstance(error, discord.NotFound):
-            await ctx.reply(embed=discord.Embed(description=str(error), color=discord.Color.red()))
-        elif isinstance(error, commands.CommandOnCooldown):
-            await ctx.reply(embed=discord.Embed(description=str(error), color=discord.Color.red()))
-        elif isinstance(error, commands.CheckFailure):
-            await ctx.reply(embed=discord.Embed(description="You are blacklisted! Contact the owner to talk about it.", color=discord.Color.red()))
+            await ctx.send(embed = discord.Embed(title = str(error), color = discord.Color.red()))
+        elif isinstance(error, discord.NotFound): await ctx.send(embed = discord.Embed(title = str(error), color = discord.Color.red()))
+        elif isinstance(error, commands.CommandOnCooldown): await ctx.send(embed = discord.Embed(title = str(error), color = discord.Color.red()))
         else:
-            c = self.bot.get_channel(770685546724982845)
+            c = bot.get_channel(#ADD YOUR CHANNEL HERE) 
             embed = discord.Embed(
-                title="An error occurred!",
-                description=f"Reported to the support server. Need more help? [Join the support server](https://penguin.vaskel.xyz/support)\n```Error: \n{str(error)}```",
-                timestamp= datetime.utcnow()
+                title = "An error occurred!",
+                description = f"Reported to the support server. Need more help? [Join the support server](https://discord.gg/7yZqHfG)\n```Error: \n{str(error)}```",
+                timestamp = datetime.datetime.utcnow()
             )
-            embed.set_footer(text=f"Caused by: {ctx.command}")
-            await ctx.reply(embed=embed)
+            embed.set_footer(text = f"Caused by: {ctx.command}")
+            await ctx.send(embed = embed)
 
-            # Support server embed
+            #Support server embed
             embed = discord.Embed(
-                title=f"An error occurred!",
-                description=f"```{str(error)}```",
-                timestamp= datetime.utcnow()
+                title = f"An error occured!",
+                description = f"```{str(error)}```",
+                timestamp = datetime.datetime.utcnow()
             )
             embed.add_field(
-                name="Details:",
-                value=f"""
+                name = "Details:",
+                value = f"""
                 Caused by: `{str(ctx.author)} [{int(ctx.author)}]`
                 In guild: `{str(ctx.guild)} [{int(ctx.guild)}]`
                 Command: `{ctx.command}`
                 """
             )
-            await c.send(embed=embed)
+            await c.send(embed = embed)
 
 def setup(bot):
     bot.add_cog(ErrorsCog(bot))
