e753c02b1520ec26ef7b0db64bbc03bef97348a8
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2020-11-30 20:21:26 -0500

    change to dedicated errors file

--- bot.py
+++ bot.py
@@ -10,8 +10,7 @@ from aiogoogletrans import Translator
 from asyncdagpi import Client
 from utils.CustomBot import PenguinBot
 
-
-startup_extensions = ['cogs.members', 'cogs.owner', 'cogs.moderator', 'cogs.fun', "jishaku", "cogs.mute", 'cogs.animals', 'cogs.listener', 'cogs.help_command', 'cogs.images']
+startup_extensions = ['cogs.members', 'cogs.owner', 'cogs.moderator', 'cogs.fun', "jishaku", "cogs.mute", 'cogs.animals', 'cogs.listener', 'cogs.help_command', 'cogs.images', 'cogs.utils.errors']
 
 os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
 os.environ["JISHAKU_HIDE"] = "True"
@@ -39,7 +38,7 @@ activity=discord.Activity(type=discord.ActivityType.listening, name="@Penguin")
 
 bot = PenguinBot(description='', 
                 intents = intents, 
-                allowed_mentions=discord.AllowedMentions(roles=False, users=True, everyone=False, replied_user = False), 
+                allowed_mentions =discord.AllowedMentions(roles=False, users=True, everyone=False, replied_user = False),
                 embed_color = 0x31A1F1,
                 activity = activity)
 
@@ -61,50 +60,6 @@ if __name__ == "__main__":
         except Exception as e:
             exc = '{}: {}'.format(type(e).__name__, e)
             print('Failed to load extension {}\n{}'.format(extension, exc))
-
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        return
-    elif isinstance(error, commands.MissingPermissions):
-        await ctx.reply(embed = discord.Embed(title = str(error), color = discord.Color.red()))
-    elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.reply(embed = discord.Embed(title = str(error), color = discord.Color.red()))
-    elif isinstance(error, commands.BadArgument):
-        await ctx.reply(embed = discord.Embed(title = str(error), color = discord.Color.red()))
-    elif isinstance(error, commands.NotOwner):
-        await ctx.reply(embed = discord.Embed(title = "You are not an owner.", color = discord.Color.red()))
-    elif isinstance(error, commands.BotMissingPermissions):
-        await ctx.reply(embed = discord.Embed(description = str(error)))
-    elif isinstance(error, discord.NotFound): await ctx.reply(embed = discord.Embed(description = str(error)))
-    elif isinstance(error, commands.CommandOnCooldown): await ctx.reply(embed = discord.Embed(description = str(error)))
-    elif isinstance(error, commands.CheckFailure): await ctx.reply(embed = discord.Embed(description = "You are blacklisted! Contact the owner to talk about it."))
-    else:
-        c = bot.get_channel(770685546724982845) 
-        embed = discord.Embed(
-            title = "An error occurred!",
-            description = f"Reported to the support server. Need more help? [Join the support server](https://penguin.vaskel.xyz/support)\n```Error: \n{str(error)}```",
-            timestamp = datetime.datetime.utcnow()
-        )
-        embed.set_footer(text = f"Caused by: {ctx.command}")
-        await ctx.reply(embed = embed)
-        
-        #Support server embed
-        embed = discord.Embed(
-            title = f"An error occured!",
-            description = f"```{str(error)}```",
-            timestamp = datetime.datetime.utcnow()
-        )
-        embed.add_field(
-            name = "Details:",
-            value = f"""
-            Caused by: `{str(ctx.author)} [{int(ctx.author)}]`
-            In guild: `{str(ctx.guild)} [{int(ctx.guild)}]`
-            Command: `{ctx.command}`
-            """
-        )
-        await c.send(embed = embed)
-
 
 botSecret = config['default']['BOT_SECRET']
 bot.nasa_api = config['default']['NASA_API']
---
+++ cogs/utils/errors.py
@@ -0,0 +1,53 @@
+import discord
+from discord.ext import commands
+from datetime import datetime
+
+class Errors(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener('on_command_error')
+    async def command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            return
+        elif isinstance(error, commands.MissingPermissions):
+            await ctx.reply(embed=discord.Embed(title=str(error), color=discord.Color.red()))
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.reply(embed=discord.Embed(title=str(error), color=discord.Color.red()))
+        elif isinstance(error, commands.BadArgument):
+            await ctx.reply(embed=discord.Embed(title=str(error), color=discord.Color.red()))
+        elif isinstance(error, commands.NotOwner):
+            await ctx.reply(embed=discord.Embed(title="You are not an owner.", color=discord.Color.red()))
+        elif isinstance(error, commands.BotMissingPermissions):
+            await ctx.reply(embed=discord.Embed(description=str(error)))
+        elif isinstance(error, discord.NotFound):
+            await ctx.reply(embed=discord.Embed(description=str(error)))
+        elif isinstance(error, commands.CommandOnCooldown):
+            await ctx.reply(embed=discord.Embed(description=str(error)))
+        elif isinstance(error, commands.CheckFailure):
+            await ctx.reply(embed=discord.Embed(description="You are blacklisted! Contact the owner to talk about it."))
+        else:
+            c = self.bot.get_channel(770685546724982845)
+            embed = discord.Embed(
+                title="An error occurred!",
+                description=f"Reported to the support server. Need more help? [Join the support server](https://penguin.vaskel.xyz/support)\n```Error: \n{str(error)}```",
+                timestamp=datetime.datetime.utcnow()
+            )
+            embed.set_footer(text=f"Caused by: {ctx.command}")
+            await ctx.reply(embed=embed)
+
+            # Support server embed
+            embed = discord.Embed(
+                title=f"An error occurred!",
+                description=f"```{str(error)}```",
+                timestamp= datetime.utcnow()
+            )
+            embed.add_field(
+                name="Details:",
+                value=f"""
+                Caused by: `{str(ctx.author)} [{int(ctx.author)}]`
+                In guild: `{str(ctx.guild)} [{int(ctx.guild)}]`
+                Command: `{ctx.command}`
+                """
+            )
+            await c.send(embed=embed)
