6823f12deba6081545f05d8cb772c4d9783c31dc
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2020-12-23 21:22:46 -0500

    fix ban and general purpose fixing of code

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -1,7 +1,5 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands.core import command
-
 
 class ModeratorCog(commands.Cog, name = "Moderator"):
     """Commands for moderators"""
@@ -14,7 +12,7 @@ class ModeratorCog(commands.Cog, name = "Moderator"):
     @commands.bot_has_permissions(kick_members = True)
     async def kick(self, ctx, member: discord.Member, *, reason = None):
         """Kicks a user, you can use `Username` `Username#discriminator` or `userId`"""
-        if member.id == ctx.author.id: return await ctx.send("You can't do that to yourself!") 
+        if member.id == ctx.author.id: return await ctx.send("You can't do that to yourself!")
         if member.top_role > ctx.me.top_role or member.top_role == ctx.me.top_role: return await ctx.send("That member's top role is higher or equal to mine!")
         try:await member.send(f"You were kicked from {ctx.guild.name} for {reason}")
         except discord.HTTPException: pass
@@ -27,12 +25,17 @@ class ModeratorCog(commands.Cog, name = "Moderator"):
     @commands.bot_has_permissions(ban_members = True)
     async def ban(self, ctx, member: discord.Member, *, reason = None):
         """Bans a user, you can use `Username` `Username#discriminator` or `userId`"""
-        if member.id == ctx.author.id: return await ctx.send("You can't do that to yourself!") 
-        if member.top_role > ctx.me.top_role or member.top_role == ctx.me.top_role: return await ctx.send("That member's top role is higher or equal to mine!")
+        if member.id == ctx.author.id: return await ctx.send("You can't do that to yourself!")
+        if member.top_role >= ctx.me.top_role: return await ctx.send("That member's top role is higher or equal to mine!")
+
         guild = ctx.guild
-        try: await member.send(f"You were banned from {guild.name} for {reason}")
-        except discord.HTTPException: pass
-        reason = f"{ctx.author} [{ctx.author.id} - " + reason
+
+        try:
+            await member.send(f"You were banned from {guild.name} for {reason}")
+
+        except discord.HTTPException:
+            pass
+        reason = f"{ctx.author} [{ctx.author.id} - {reason}"
         await guild.ban(member, reason = reason)
         await ctx.send(f"{member.name} was banned for {reason}.")
 
@@ -42,9 +45,9 @@ class ModeratorCog(commands.Cog, name = "Moderator"):
     @commands.bot_has_permissions(ban_members = True)
     async def unban(self, ctx, user: int, *, reason = None):
         """Unbans a user with a given ID"""
-        if user == ctx.author.id: return await ctx.send("You can't do that to yourself!") 
+        if user == ctx.author.id: return await ctx.send("You can't do that to yourself!")
         member = discord.Object(id = user)
-        try: 
+        try:
             await ctx.guild.unban(member)
             await ctx.send(embed = discord.Embed(description = f"Unbanned {member.id} for {reason}."))
         except discord.NotFound: return await ctx.send(embed = discord.Embed(description = "That user doesn't seem to be banned."))
@@ -66,71 +69,75 @@ class ModeratorCog(commands.Cog, name = "Moderator"):
     @commands.bot_has_permissions(manage_roles = True)
     @commands.guild_only()
     @commands.command()
-    async def block(self, ctx, user: discord.Member = None):
+    async def block(self, ctx, user: discord.Member):
         """Blocks a user from sending messages in the channel"""
-        if user == ctx.author.id: return await ctx.send("You can't do that to yourself!") 
-        if user.top_role > ctx.me.top_role or user.top_role == ctx.me.top_role: return await ctx.send("That member's top role is higher or equal to mine!")
-        if user.top_role > ctx.author.top_role or user.top_role == ctx.author.top_role: return await ctx.send("That member's top role is higher or equal to yours!")
+        if user == ctx.author.id: return await ctx.send("You can't do that to yourself!")
+        if user.top_role >= ctx.me.top_role: return await ctx.send("That member's top role is higher or equal to mine!")
+        if user.top_role >= ctx.me.top_role: return await ctx.send("That member's top role is higher or equal to yours!")
         channel = ctx.channel
-        if user == None:
-            await ctx.send("User not specified, try again.")
-        
+
         await channel.set_permissions(user, send_messages=False)
         await ctx.send(f"{user} was blocked from the channel.")
-    
+
     @commands.has_permissions(manage_roles = True)
     @commands.bot_has_permissions(manage_roles = True)
     @commands.guild_only()
     @commands.command()
-    async def unblock(self, ctx, user: discord.Member = None):
+    async def unblock(self, ctx, user: discord.Member):
         """Unblocks the given user from the channel:"""
         if user.top_role > ctx.me.top_role or user.top_role == ctx.me.top_role: return await ctx.send("That member's top role is higher or equal to mine!")
         if user.top_role > ctx.author.top_role or user.top_role == ctx.author.top_role: return await ctx.send("That member's top role is higher or equal to yours!")
         channel = ctx.channel
-        if user == None:
-            await ctx.send("User not specified, try again.")
-        
+
         await channel.set_permissions(user, send_messages=True)
         await ctx.send(f"{user} was unblocked from the channel.")
-    
+
     @commands.has_permissions(manage_channels = True)
     @commands.bot_has_permissions(manage_channels = True)
     @commands.guild_only()
     @commands.command(help = "Adds a slowmode to a channel in seconds, use no arg or 0 to remove slowmode.")
     async def slowmode(self, ctx, seconds: int = None):
-        channel = ctx.channel 
-    
-        if seconds == None:
+        channel = ctx.channel
+
+        if not seconds:
             await channel.edit(slowmode_delay=0)
-        
+
         await channel.edit(slowmode_delay=seconds)
 
     @commands.has_permissions(manage_guild = True)
-    @commands.command(description = "Leaves guild, only useable by users with the administrator role.")
+    @commands.command(description = "Leaves guild, only usable by users with the `manage_guild` permission.")
     async def leave(self, ctx):
         await ctx.send(f"Leaving server {ctx.guild.name}, goodbye.")
         print(f"Left server {ctx.guild.name}")
         await ctx.guild.leave()
-    
+
     @commands.command(description = "This bans a user, but saves their messages.")
     @commands.guild_only()
     @commands.has_permissions(ban_members = True)
     @commands.bot_has_permissions(ban_members = True)
     async def saveban(self, ctx, member: discord.Member, *, reason = None):
-        if member.id == ctx.author.id: return await ctx.send("You can't do that to yourself!") 
-        if member.top_role > ctx.me.top_role or member.top_role == ctx.me.top_role: return await ctx.send("That member's top role is higher or equal to mine!")
+        if member.id == ctx.author.id:
+            return await ctx.send("You can't do that to yourself!")
+
+        if member.top_role >= ctx.me.top_role:
+            return await ctx.send("That member's top role is higher or equal to mine!")
+
         guild = ctx.guild
-        reason = f"{ctx.author} [{ctx.author.id} - " + reason
-        try: await member.send(f"You were banned from {guild.name} for {reason}")
-        except discord.HTTPException: pass
-        await guild.ban(member, delete_message_days = 0, reason = reason)
-        await ctx.send(f"{member.name} was banned for {reason}.")
-    
+        audit = f"{ctx.author} [{ctx.author.id}] - {reason}"
+        try:
+            await member.send(
+            embed = discord.Embed(description=f"You were banned from {guild.name} for {audit}")
+            )
+        except discord.HTTPException:
+            pass
+        await guild.ban(member, delete_message_days = 0, reason = audit)
+        await ctx.send(f"{member.name} was banned for {audit}.")
+
     @commands.command()
     @commands.guild_only()
     @commands.has_permissions(manage_guild = True)
     async def set_prefix(self, ctx, newPrefix: str):
-        
+
         if not ctx.guild.id in self.bot.prefixes:
             await self.bot.db.execute('INSERT INTO guild_config(id, prefix) VALUES($1, $2)', ctx.guild.id, newPrefix)
         elif ctx.guild.id in self.bot.prefixes:
@@ -138,7 +145,7 @@ class ModeratorCog(commands.Cog, name = "Moderator"):
 
         self.bot.prefixes = dict(await self.bot.db.fetch("SELECT id, prefix FROM guild_config"))
         await ctx.send(embed = discord.Embed(description = f"Set {ctx.guild}'s prefix to {newPrefix}."))
-    
+
     @commands.guild_only()
     @commands.group(name="reactionroles", aliases = ['reaction_roles', 'reactionrole', 'reaction_role'])
     async def reactionroles(self, ctx):
@@ -156,7 +163,7 @@ class ModeratorCog(commands.Cog, name = "Moderator"):
         await self.bot.db.execute("INSERT INTO reaction_roles(msg_id, role_id, guild_id) VALUES ($1, $2, $3)", message.id, role.id, ctx.guild.id)
         self.bot.reactionRoleDict = dict(await self.bot.db.fetch("SELECT msg_id, role_id FROM reaction_roles"))
         await ctx.send(embed = discord.Embed(description = f"Added a reaction role to {message.id} that gives role {role.name}"))
-    
+
     @reactionroles.command(help = "Removes a reaction role from the given message id", name = 'remove')
     @commands.guild_only()
     @commands.has_permissions(manage_roles = True)
@@ -172,14 +179,14 @@ class ModeratorCog(commands.Cog, name = "Moderator"):
         for entry in await self.bot.db.fetch("SELECT msg_id, role_id FROM reaction_roles WHERE guild_id = $1", ctx.guild.id):
             l += f"Message ID: {entry[0]} Role ID: {entry[1]}\n"
         await ctx.send(embed = discord.Embed(description = l, color = discord.Color.from_rgb(48,162,242)))
-    
+
     @commands.guild_only()
     @commands.group(name="log")
     async def log_group(self, ctx):
         """A command that deals with the bots logging."""
         if ctx.invoked_subcommand is None:
             await ctx.send_help(ctx.command)
-    
+
     @log_group.command(name = 'set')
     @commands.guild_only()
     @commands.has_permissions(manage_guild = True)
@@ -218,7 +225,7 @@ class ModeratorCog(commands.Cog, name = "Moderator"):
         """Command group that deals with welcoming"""
         if ctx.invoked_subcommand is None:
             await ctx.send("No subcommand passed, use `help welcomer` for help.")
-    
+
     @welcomer_group.command(name = "set", aliases = ["set_channel"])
     @commands.guild_only()
     @commands.has_permissions(manage_guild = True)
@@ -230,8 +237,8 @@ class ModeratorCog(commands.Cog, name = "Moderator"):
         elif ctx.guild.id in self.bot.welcome_dict:
             await self.bot.db.execute("UPDATE guild_config SET welcomeid = $1 WHERE id = $2", channel.id, ctx.guild.id)
         await ctx.send(embed= discord.Embed(description = f"Set {channel.mention} as the welcoming channel."))
-        
-    
+
+
     @welcomer_group.command(name = "set_message")
     @commands.guild_only()
     @commands.has_permissions(manage_guild = True)
@@ -248,7 +255,7 @@ class ModeratorCog(commands.Cog, name = "Moderator"):
         await self.bot.db.execute("UPDATE guild_config SET welcomeMessage = $1 WHERE id = $2", message, ctx.guild.id)
         await ctx.send(f"Changed the welcome message to ```{message}```")
         self.bot.cache[ctx.guild.id]["welcomeMessage"] = message
-    
+
     @welcomer_group.command(name = "delete", aliases = ["remove_channel"])
     @commands.guild_only()
     @commands.has_permissions(manage_guild = True)
@@ -258,7 +265,7 @@ class ModeratorCog(commands.Cog, name = "Moderator"):
         await ctx.send(embed = discord.Embed(description = f"Removed the welcome channel and disabled welcomer."))
         self.bot.cache[ctx.guild.id]["welcomeEnabled"] = False
         self.bot.cache[ctx.guild.id]["welcomeId"] = None
-    
+
     @welcomer_group.command(name = "disable")
     @commands.guild_only()
     @commands.has_permissions(manage_guild = True)
@@ -282,7 +289,7 @@ class ModeratorCog(commands.Cog, name = "Moderator"):
     async def autorole_group(self, ctx):
         """Autorole commands"""
         if ctx.invoked_subcommand is None: await ctx.send("No subcommand passed, use `help autorole` for help.")
-    
+
     @autorole_group.command(name = "add")
     @commands.guild_only()
     @commands.has_permissions(manage_roles= True)
@@ -292,7 +299,7 @@ class ModeratorCog(commands.Cog, name = "Moderator"):
         if role > ctx.guild.me.top_role or role == ctx.guild.me.top_role: return await ctx.send("That role is either above me or is my top role!")
         await self.bot.db.execute("UPDATE guild_config SET autorole = $1 WHERE id = $2", role.id, ctx.guild.id)
         await ctx.send(embed = discord.Embed(description = f"Added autorole for role {role.mention}."))
-    
+
     @autorole_group.command(name = "remove")
     @commands.guild_only()
     @commands.has_permissions(manage_roles= True)
@@ -300,7 +307,7 @@ class ModeratorCog(commands.Cog, name = "Moderator"):
         """Removes guilds autorole"""
         await self.bot.db.execute("UPDATE guild_config SET autorole = null WHERE id = $1", ctx.guild.id)
         await ctx.send(embed = discord.Embed(description = f"Removed autorole"))
-    
+
     @autorole_group.command(name = "role")
     @commands.guild_only()
     @commands.has_permissions(manage_roles= True)
