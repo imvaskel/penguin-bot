804c1cba7ac614761bfac0ba01cb48d99de7e2df
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2020-12-31 17:57:37 -0500

    ipc stuffs

--- utils/ipc.py
+++ utils/ipc.py
@@ -9,12 +9,14 @@ class IpcRoutes(commands.Cog):
 
     @ipc.server.route()
     async def refresh_cache_for_guild(self, data):
-        guild = int(data.guild_id)  # get the guild object using parsed guild_id
+        guild = data.guild_id
+        guild = int(guild)
 
         try:
             await self.bot.refresh_cache_for(guild)
             return "done"
-        except:
+        except Exception as e:
+            print(e)
             return "error"
 
 
