3f49d5d3da8dc96bd69e603628d057baa92b2584
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2021-01-09 13:40:33 -0500

    custom context testing

--- utils/CustomContext.py
+++ utils/CustomContext.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 
 class PenguinContext(commands.Context):
 
-    @property
     async def embed(self, *args, **kwargs):
         """Sends an embed with the args given"""
         embed = discord.Embed(**kwargs)
