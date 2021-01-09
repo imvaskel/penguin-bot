49fac05109b180baf2edc8290e8baccca86e3a0b
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2021-01-09 13:52:48 -0500

    add color to ctx.embed

--- utils/CustomContext.py
+++ utils/CustomContext.py
@@ -6,7 +6,7 @@ class PenguinContext(commands.Context):
 
     async def embed(self, *args, **kwargs):
         """Sends an embed with the args given"""
-        embed = discord.Embed(**kwargs, timestamp=datetime.utcnow())
+        embed = discord.Embed(**kwargs, color=0x36393E, timestamp=datetime.utcnow())
         embed.set_footer(icon_url=str(self.author.avatar_url), text=f"Requested by {self.author}")
         await self.send(embed = embed)
 
