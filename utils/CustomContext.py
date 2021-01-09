e9a277b1dd271cfe94e02649bb6c47a7092ae2e7
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2021-01-09 13:45:37 -0500

    custom context testing

--- utils/CustomContext.py
+++ utils/CustomContext.py
@@ -1,10 +1,12 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
 
 class PenguinContext(commands.Context):
 
     async def embed(self, *args, **kwargs):
         """Sends an embed with the args given"""
-        embed = discord.Embed(**kwargs)
+        embed = discord.Embed(**kwargs, timestamp=kwargs.get("timestamp") or datetime.utcnow())
+        embed.set_footer(text=f"Requested by {self.author}")
         await self.send(embed = embed)
 
