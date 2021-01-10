465048564e9001d4a01574c1a9e2f46e3d074a08
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2021-01-09 21:32:28 -0500

    try to fix cog thing

--- cogs/checks.py
+++ cogs/checks.py
@@ -2,11 +2,17 @@ import discord
 from discord.ext import commands
 from utils.CustomErrors import *
 from utils.CustomBot import PenguinBot
+from listeners.errors import ErrorEmbed
 
 class CheckCog(commands.Cog):
     def __init__(self, bot: PenguinBot):
         self.bot = bot
         bot.add_check(self.isBlacklisted)
+
+    async def cog_command_error(self, ctx, error):
+        if isinstance(error, Blacklisted):
+            await ctx.reply(embed=ErrorEmbed(description=str(error)))
+
 
     async def isBlacklisted(self, ctx):
         if ctx.author.id in self.bot.blacklistedUsers:
