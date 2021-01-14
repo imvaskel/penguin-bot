3c30ffb0f311d462dcab8aa727ce5080f4bfd480
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2021-01-14 13:07:53 -0500

    update ctx.embed to allow customization

--- utils/CustomContext.py
+++ utils/CustomContext.py
@@ -6,8 +6,14 @@ class PenguinContext(commands.Context):
 
     async def embed(self, *args, **kwargs):
         """Sends an embed with the args given"""
-        embed = discord.Embed(**kwargs, color=0x36393E, timestamp=datetime.utcnow())
+        title = kwargs.get('title')
+        description = kwargs.get('description')
+        color = kwargs.get('color', 0x36393E)
+        timestamp = kwargs.get('timestamp', datetime.utcnow())
+        embed = discord.Embed(title=title, description=description, color=color, timestamp=timestamp)
+
         embed.set_footer(icon_url=str(self.author.avatar_url), text=f"Requested by {self.author}")
+
         await self.send(embed = embed)
 
     async def refresh_cache(self):
