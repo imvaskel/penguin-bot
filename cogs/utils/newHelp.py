c9f52a4b8a03274e3fd2db0fe626d90549e6a0a8
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2021-01-19 13:44:36 -0500

    i might be dumb

--- cogs/utils/newHelp.py
+++ cogs/utils/newHelp.py
@@ -8,7 +8,6 @@ class PenguinHelp(commands.HelpCommand):
         await channel.send(embed=discord.Embed(description=string))
 
     async def send_bot_help(self, mapping):
-        del mapping['IpcRoutes']
         filtered_commands = {key: await self.filter_commands(value) for key, value in mapping.items() if key.qualifed_name != "IpcRoutes"}
         embed = discord.Embed(title = "Help",
                               description=f"Use `{self.clean_prefix}` help [command] or [module] for more help.")
