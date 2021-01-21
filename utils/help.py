c5063dc6d1f510582aa2892a0ed6c74d4840e6a7
Vaskel <vaskel@vaskel.xyz>
2021-01-21 13:31:39 -0500

    help finished

--- utils/help.py
+++ utils/help.py
@@ -1,13 +1,29 @@
 import discord
 from discord.ext import commands, menus
-from listeners.errors import ErrorEmbeds
 
 class FormatCogHelp:
-    def __init__(self, command: commands):
+    def __init__(self, command: commands.Command):
         formatted = f"[{' | '.join([alias for alias in command.aliases])}]" if command.aliases else ""
         formatted += "\nNone" if not command.help else "\n" + command.help
         self.formatted = (command.name, formatted)
         self.cog_name = command.cog_name
+
+class GroupHelpSource(menus.ListPageSource):
+    def __init__(self, data):
+        super().__init__(data, per_page=5)
+
+    async def format_page(self, menu, entries):
+        offset = menu.current_page * self.per_page
+        embed = discord.Embed(title = entries[0].root_parent.qualified_name,
+                              color=0x36393E)
+
+        for index, command in enumerate(entries, start=offset):
+            embed.add_field(name=command.qualified_name,
+                            value=(
+                                f"[{' | '.join([alias for alias in command.aliases])}] \n" if command.aliases else ""
+                                f"{command.help or 'None'}"
+                            ))
+        return embed
 
 class CogHelpSource(menus.ListPageSource):
     def __init__(self, data):
@@ -15,13 +31,15 @@ class CogHelpSource(menus.ListPageSource):
 
     async def format_page(self, menu, entries):
         offset = menu.current_page * self.per_page
-        embed = discord.Embed(title=entries[0].cog_name)
+        embed = discord.Embed(title=entries[0].cog_name,
+                              color= 0x36393E)
 
         for i, v in enumerate(entries, start=offset):
             command = v.formatted
             embed.add_field(name = command[0],
                             value = command[1],
                             inline=False)
+        embed.set_footer(text = f"Page {menu.current_page+1} / {self.get_max_pages()}")
         return embed
 
 class CogHelpPages(menus.MenuPages):
@@ -36,13 +54,13 @@ class PenguinHelp(commands.HelpCommand):
         })
 
     async def command_not_found(self, string):
-        channel = self.get_destination()
-        await channel.send(embed=discord.Embed(description=f"The command `{string}` was not found."))
+        return f"The command `{string}` was not found."
 
     async def send_bot_help(self, mapping):
         filtered_commands = {key: await self.filter_commands(value) for key, value in mapping.items() if getattr(key, "qualified_name", "None") != "IpcRoutes"}
         embed = discord.Embed(title = "Help",
-                              description=f"Use `{self.clean_prefix}help` [command] or [module] for more help.")
+                              description=f"Use `{self.clean_prefix}help` [command] or [module] for more help.",
+                              color=0x36393E)
         for cog, cmds in filtered_commands.items():
             if cmds:
                 embed.add_field(name = getattr(cog, "qualified_name", "None"),
@@ -58,8 +76,9 @@ class PenguinHelp(commands.HelpCommand):
         embed = discord.Embed(title= command.qualified_name,
                               description = ("`<arg>` This is a required arg \n"
                                              "`[arg]` This is optional \n"
-                                             "`[arg...]` This can have multiple args"))
-        command = (await self.filter_commands([command]))
+                                             "`[arg...]` This can have multiple args"),
+                              color=0x36393E)
+        command = (await self.filter_commands([command]))[0]
 
         if not command:
             return await self.get_destination().send(embed = embed)
@@ -74,3 +93,7 @@ class PenguinHelp(commands.HelpCommand):
                         value= command.signature or "None",
                         inline=False)
         await self.get_destination().send(embed = embed)
+
+    async def send_group_help(self, group: commands.Group):
+        menu = CogHelpPages(source=GroupHelpSource([command for command in group.commands]))
+        await menu.start(self.context)
